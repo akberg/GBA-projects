be80a8319c9192d9f9b0962602feddcf89063c43
Andreas K. Berg <andreaskb98@gmail.com>
2020-11-05 00:16:08 +0100

    template

---
+++ snake/source/template.c
@@ -0,0 +1,35 @@
+
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//---------------------------------------------------------------------------------
+// Program entry point
+//---------------------------------------------------------------------------------
+// int main(void) {
+// //---------------------------------------------------------------------------------
+
+// 	int a = round(5.6);
+// 	// the vblank interrupt must be enabled for VBlankIntrWait() to work
+// 	// since the default dispatcher handles the bios flags no vblank handler
+// 	// is required
+// 	irqInit();
+// 	irqEnable(IRQ_VBLANK);
+
+// 	consoleDemoInit();
+
+// 	// ansi escape sequence to set print co-ordinates
+// 	// /x1b[line;columnH
+// 	iprintf("\x1b[10;10HHello World!\n");
+
+// 	while (1) {
+// 		VBlankIntrWait();
+// 	}
+// }
+
+
